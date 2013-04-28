56fbdefc00c40c0734cfc5432ea0b5fc78af3d51
Jonathan Rudenberg <jonathan@titanous.com>
2013-04-28 13:49:26 -0400

    Add basic app create example

--- examples.go
+++ examples.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"os"
 
@@ -9,13 +10,30 @@ import (
 )
 
 var meta *tent.MetaPost
+var client *tent.Client
 
 func discover() []*request {
 	var err error
 	meta, err = tent.Discover(os.Args[1])
-	if err != nil {
-		panic(err)
-	}
+	maybePanic(err)
+	client = &tent.Client{Servers: meta.Servers}
+	return getRequests()
+}
+
+func createApp() []*request {
+	post := tent.NewAppPost(&tent.App{
+		Name: "Example App",
+		URL:  "https://app.example.com",
+		PostTypes: tent.AppPostTypes{
+			Write: []string{"https://tent.io/types/post/v0"},
+			Read:  []string{"https://tent.io/types/app/v0"},
+		},
+		RedirectURI: "https://app.example.com/oauth",
+	})
+	err := client.CreatePost(post)
+	maybePanic(err)
+	_, err = post.GetCredentials()
+	maybePanic(err)
 	return getRequests()
 }
 
@@ -27,6 +45,10 @@ func main() {
 	examples["discover_head"] = discoveryReqs[0]
 	examples["discover_meta"] = discoveryReqs[1]
 
+	appReqs := createApp()
+	examples["app_create"] = appReqs[0]
+	examples["app_credentials"] = appReqs[1]
+
 	res := make(map[string]string)
 	for k, v := range examples {
 		res[k] = requestMarkdown(v)
@@ -34,4 +56,13 @@ func main() {
 
 	data, _ := json.Marshal(res)
 	ioutil.WriteFile(os.Args[2], data, 0644)
+}
+
+func maybePanic(err error) {
+	if err != nil {
+		if resErr, ok := err.(*tent.BadResponseError); ok && resErr.TentError != nil {
+			fmt.Println(resErr.TentError)
+		}
+		panic(err)
+	}
 }
